4920286be1d362b74e932eb80ace397805d485a3
brekk <brekk@brekkbockrath.com>
2017-10-23 14:37:28 -0700

    needs robustifying, but works

--- README.md
+++ README.md
@@ -7,7 +7,6 @@
 ```
 
 **ANGular-React**?
-**Unhappy**?
 
 Gimme three thingies:
 
@@ -17,37 +16,6 @@ Gimme three thingies:
 
 I'll give you back a poor man's React component.
 
-Underlying things:
+# Usage
 
-  * `htmltojsx`
-    - handle some of the auto-conversion of html to jsx (`ng-`-attributes retaining utility will likely be a problem)
-    - we still need to wrap this with a template:
-
-    ```
-    import * as angular from 'angular'
-    import * as react2angular from 'react2angular'
-    import './{{hyphenCase name}}.scss'
-    //import './{{hyphenCase name}}.html'
-    // import other stuff here
-
-    // these are the typescript proptypes, effectively
-    export interface {{pascalCase name}}PropTypes {
-      {{types}}
-    }
-    // these props are used by react2angular and various other downstream consumers
-    export const propTypes = {{JSON propTypes}}
-
-    // this is our react component
-    export const {{pascalCase name}}(props: {{pascalCase name}}PropTypes): typeof React.Component => {
-      return ({{convertedJSX}})
-    }
-    // needs propTypes for react2angular to work
-    {{pascalCase name}}.propTypes = propTypes
-
-    // let's export the angular nonsense
-    export const DistributeAngular{{pascalCase name}} = angular.module(`distribute.components.{{hyphenCase name}}`, [])
-      .component(`{{name}}`, react2angular({{pascalCase name}}))
-    ```
-
-Future:
-* `css-to-json` ->
+Run `plop` in the root directory (for now).
--- plopfile.js
+++ plopfile.js
@@ -1,5 +1,6 @@
 const fs = require(`fs`)
-const {curry} = require(`f-utility`)
+const {join, fromPairs, I, K, curry, pipe, reduce, map, split, merge} = require(`f-utility`)
+const {trace} = require(`xtrace`)
 const HTML2JSX = require(`htmltojsx`)
 const directory = require(`inquirer-file-path`)
 
@@ -12,17 +13,78 @@ const regexValidate = curry((regex, name, expected) => (
 const shouldExist = regexValidate(/.+/)
 const shouldBeANumber = regexValidate(/\d+/)
 
+const prepend = curry((before, x) => before + x)
+const append = curry((after, x) => x + after)
+
+const addRequirement = curry((k, v) => (
+  k[k.length - 1] === `?` ?
+  v :
+  append(`.isRequired`, v)
+))
+
+const unchangedTypes = [
+  `boolean`,
+  `string`,
+  `any`,
+  `void`,
+  `number`
+]
+const convertTSToPropTypes = pipe(
+  (v) => {
+    if (v === `object`) {
+      return `any`
+    }
+    if (unchangedTypes.includes(v)) {
+      return v
+    }
+    if (v.indexOf(`[]`) > -1) {
+      return `arrayOf(PropTypes.${v})`
+    }
+  },
+  prepend(`PropTypes.`)
+)
+
+const doubleSplit = curry((d1, d2, x) => {
+  if (!x) {
+    return []
+  }
+  if (x.indexOf(d1) === -1 || x.indexOf(d2) === -1) {
+    return []
+  }
+  return map(split(d2), split(d1, x) || [])
+})
+
+const j2 = (x) => JSON.stringify(x, null, 2)
+const reduceToStrings = reduce((x, [k, v]) => x + `\n  ${k}: ${v},`, ``)
+
 module.exports = function templatedComponentGenerator(plop) {
+  plop.setHelper(`proptypes`, pipe(
+    doubleSplit(`,`, `:`),
+    map(([k, v]) => [k, addRequirement(k, convertTSToPropTypes(v))]),
+    reduceToStrings,
+    (x) => x.substr(0, x.length - 1)
+  ))
+  plop.setHelper(`typescript`, (props) => {
+    const converted = pipe(
+      // split(`,`),
+      doubleSplit(`,`, `:`),
+      reduceToStrings
+      // map(([k, v]) => `  ${k}: ${v}`),
+      // join(`\n`),
+      // trace(`what is this?!?!?!`)
+    )(props)
+    // console.log(`props`, props, converted)
+    return converted.substr(0, converted.length - 1)
+    // return props
+  })
   plop.setHelper(`html2jsx`, (name, template) => {
     const hatemail = fs.readFileSync(template, `utf8`)
     const converter = new HTML2JSX({
       indent: `  `,
       hideComment: true,
       createClass: false
-      // outputClassName: `{{pascalCase name}}`
     })
-    const jsx = converter.convert(hatemail)
-    return jsx
+    return converter.convert(hatemail)
   })
   plop.setPrompt(`directory`, directory)
   plop.setGenerator(`react2angular`, {
@@ -40,6 +102,12 @@ module.exports = function templatedComponentGenerator(plop) {
         message: `Where is the html template for this component stored?`,
         validate: shouldExist(`template`),
         basePath: plop.getPlopfilePath()
+      },
+      {
+        type: `input`,
+        name: `types`,
+        message: `Supply a string that represents proptypes / interface: a:number,b:any,c:string`,
+        validate: K(true)
       }
     ],
     actions: [
