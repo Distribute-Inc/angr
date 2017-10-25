90b61435d638b12df54754bc134baa2fdd7c2ee7
brekk <brekk@brekkbockrath.com>
2017-10-25 08:29:27 -0700

    cleaner

--- plopfile.js
+++ plopfile.js
@@ -1,129 +1,185 @@
 const fs = require(`fs`)
 const {
-  trim,
+  // I,
+  alterFirstIndex,
   alterLastIndex,
-  K,
   curry,
+  K,
+  map,
   pipe,
   reduce,
-  map,
-  split
+  split,
+  trim
 } = require(`f-utility`)
-const thingHas = curry((thing, x) => thing.includes(x))
 // const {trace} = require(`xtrace`)
 const HTML2JSX = require(`htmltojsx`)
-const directory = require(`inquirer-file-path`)
-
-const regexValidate = curry((regex, name, expected) => (
-  regex.test(expected) ?
-  true :
-  `${name} is expected`
-))
-
-const shouldExist = regexValidate(/.+/)
-// const shouldBeANumber = regexValidate(/\d+/)
-
-const prepend = curry((before, x) => before + x)
-const append = curry((after, x) => x + after)
-
-const removeLast = (x) => x.substr(0, x.length - 1)
-
-const addRequirement = ([k, v]) => {
-  const last = k.length - 1
-  const isCurious = k[last] === `?`
-  return (
-    isCurious ?
-    [removeLast(k), v] :
-    [k, append(`.isRequired`, v)]
-  )
-}
-
-const unchangedTypes = [
-  `boolean`,
-  `string`,
-  `any`,
-  `void`,
-  `number`
-]
-const convertTSToPropTypes = pipe(
-  (v) => {
-    if (v === `object`) {
-      return `any`
-    }
-    if (unchangedTypes.includes(v)) {
-      return v
-    }
-    if (v.indexOf(`[]`) > -1) {
-      return `arrayOf(PropTypes.${v})`
-    }
-  },
-  prepend(`PropTypes.`)
-)
-
-/*
- this enforces the pattern we use to get props / types from the command-line
- input: "a:number,b:string,c?:any,d:boolean"
- output: [
-   [`a`, `number`],
-   [`b`, `string`],
-   [`c?`, `any`],
-   [`d`, `boolean`]
- ]
- */
-const doubleSplit = curry((d1, d2, x) => {
-  // check to see if there's a prop on x
-  const has = thingHas(x)
-  // shortcut early
-  if (!x || !has(d1) || !has(d2)) {
-    return []
-  }
-  // like x.split(d1).map((y) => y.split(d2)) but cleaner
-  return pipe(
-    trim,
-    split(d1),
-    map(pipe(
-      trim,
-      split(d2),
-      map(trim)
-    ))
-  )(x)
-})
-
-const j2 = (x) => JSON.stringify(x, null, 2)
-const reduceToStrings = reduce((x, [k, v]) => x + `\n  ${k}: ${v},`, ``)
+const filePath = require(`inquirer-file-path`)
 
 module.exports = function templatedComponentGenerator(plop) {
-  plop.setHelper(`proptypes`, pipe(
-    // split first on commas, then colons
-    doubleSplit(`,`, `:`),
+  const thingHas = curry((thing, x) => thing.includes(x))
+  const regexValidate = curry((regex, name, expected) => (
+    regex.test(expected) ?
+    true :
+    `${name} is expected`
+  ))
+
+  const shouldExist = regexValidate(/.+/)
+  // const shouldBeANumber = regexValidate(/\d+/)
+
+  const prepend = curry((before, x) => before + x)
+  const append = curry((after, x) => x + after)
+
+  // removeFromEndOfString(1, 'what!') => 'what'
+  // removeFromEndOfString(-1, 'what!') => 'what'
+  // removeFromEndOfString(-2, 'what!') => 'wha'
+  const removeFromEndOfString = curry((offset, x) => x.substr(0, x.length - Math.abs(offset)))
+  // removeLast('what!') => 'what'
+  const removeLast = removeFromEndOfString(-1)
+  const removeLastTwo = removeFromEndOfString(-2)
+
+  /**
+   * like x.split(d1).map((y) => y.split(d2)) but cleaner
+   * this enforces the pattern we use to get props / types from the command-line
+   * @function splitTwice
+   * @param {String} d1 - first delimiter to split on
+   * @param {String} d2 - second delimiter to split on
+   * @param {String} x - a string that looks like "crap:cool,butts:what" (or other delimiters)
+   * @returns {Array} the original string, but split first on d1, then split on d2 (and trimmmed)
+   * @example
+   * const input = "a:number,b:string,c?:any,d:boolean"
+   * splitTwice(input)
+   * // prints: [
+   * //   [`a`, `number`],
+   * //   [`b`, `string`],
+   * //   [`c?`, `any`],
+   * //   [`d`, `boolean`]
+   * // ]
+   */
+  const splitTwice = curry((d1, d2, x) => {
+    // check to see if there's a prop on x
+    const has = thingHas(x)
+    // shortcut early
+    if (!x || !has(d1) || !has(d2)) {
+      return []
+    }
+    return pipe(
+      trim,
+      split(d1),
+      map(pipe(
+        trim,
+        split(d2),
+        map(trim)
+      ))
+    )(x)
+  })
+  const DELIMITERS = Object.freeze({
+    COMMA: `,`,
+    COLON: `:`
+  })
+
+  const splitOnCommasThenColons = splitTwice(DELIMITERS.COMMA, DELIMITERS.COLON)
+
+  // this is what ultimately formats the content that we print in the proptypes / interface
+  const formatLines = curry(
+    (delim, arr) => reduce(
+      (x, [k, v]) => x + `\n  ${k}: ${v}${delim}`,
+      ``,
+      arr
+    )
+  )
+
+  // # typescript
+
+  plop.setHelper(`typescript`, pipe(
+    splitOnCommasThenColons,
+    // silly typescript likes semi-colons
+    formatLines(`;`)
+  ))
+
+  /**
+   * a clunky but workable typescript-to-proptype conversion
+   * limited. doesn't do complex types yet
+   * @function convertTSToPropTypes
+   * @param {String} v - a string representing a typescript literal
+   * @returns {String} a string that is the proptype representation of the same thing (hopefully?)
+   */
+  const convertTSToPropTypes = pipe(
+    (v) => {
+      // later maybe we can add shapes or whatever
+      if (v === `object`) {
+        return `any`
+      }
+      // silly proptypes, making me remember this
+      if (v === `boolean`) {
+        return `bool`
+      }
+      const unchangedTypes = [
+        `string`,
+        `any`,
+        `number`
+      ]
+      if (unchangedTypes.includes(v)) {
+        return v
+      }
+      if (v.indexOf(`[]`) > -1) {
+        return `arrayOf(PropTypes.${removeLastTwo(v)})`
+      }
+    },
+    prepend(`PropTypes.`)
+  )
+
+  // addRequirement(['butts?', 'PropTypes.number']) => [`butts`, `PropTypes.number`]
+  // addRequirement(['butts', 'PropTypes.number']) => [`butts`, `PropTypes.number.isRequired`]
+  const addRequirement = ([k, v]) => {
+    const last = k.length - 1
+    const isCurious = k[last] === `?`
+    return (
+      isCurious ?
+      [removeLast(k), v] :
+      [k, append(`.isRequired`, v)]
+    )
+  }
+
+  const fixArray = (k) => (
+    k.indexOf(`[]`) > -1 ?
+      removeLastTwo(k) :
+      k
+  )
+
+  const propTypify = map(pipe(
     // convert TS tuples to proptypes,
-    map(alterLastIndex(convertTSToPropTypes)),
+    alterLastIndex(convertTSToPropTypes),
     // add `isRequired` to any key that ends in a ?
-    map(addRequirement),
+    addRequirement,
+    // remove `[]` from any array key
+    alterFirstIndex(fixArray)
+  ))
+
+  plop.setHelper(`proptypes`, pipe(
+    splitOnCommasThenColons,
+    // convert typescript interface dealies to proptypes
+    propTypify,
     // convert to a writeable format
-    reduceToStrings,
+    formatLines(DELIMITERS.COMMA),
     // remove the last character, which is a comma
     removeLast
   ))
-  plop.setHelper(`typescript`, pipe(
-      // split first on commas, then colons
-      doubleSplit(`,`, `:`),
-      // convert to a writeable format
-      reduceToStrings,
-      // remove the last character, which is a comma
-      removeLast
-    )
-  )
-  plop.setHelper(`html2jsx`, (name, template) => {
-    const hatemail = fs.readFileSync(template, `utf8`)
-    const converter = new HTML2JSX({
-      indent: `  `,
-      hideComment: true,
-      createClass: false
-    })
-    return converter.convert(hatemail)
+
+  // make a hatemail converter
+  const hatemailToJSX = new HTML2JSX({
+    indent: `  `,
+    hideComment: true,
+    createClass: false
   })
-  plop.setPrompt(`directory`, directory)
+  plop.setHelper(`html2jsx`, (name, template) => {
+    // grab our template
+    // TODO: make this async?
+    const hatemail = fs.readFileSync(template, `utf8`)
+    return hatemailToJSX.convert(hatemail)
+  })
+  // this is our file inspector prompt
+  plop.setPrompt(`filePath`, filePath)
+  // this is our core generator
   plop.setGenerator(`react2angular`, {
     description: `generate a component which works in both React & Angular`,
     prompts: [
@@ -134,7 +190,7 @@ module.exports = function templatedComponentGenerator(plop) {
         validate: shouldExist(`name`)
       },
       {
-        type: `directory`,
+        type: `filePath`,
         name: `template`,
         message: `Where is the html template for this component stored?`,
         validate: shouldExist(`template`),
